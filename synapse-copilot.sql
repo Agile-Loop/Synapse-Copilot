bb2f5df9cfdf072205644e5d05d777fb37a4eeb8
hmusman <hmusman.it@gmail.com>
2024-06-15 11:59:31 +0500

    Update synapse-copilot.sql

--- synapse-copilot.sql
+++ synapse-copilot.sql
@@ -3,13 +3,19 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 19, 2024 at 07:23 AM
+-- Generation Time: Jun 15, 2024 at 06:38 AM
 -- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- PHP Version: 8.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `synapse-copilot`
@@ -22,15 +28,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `credentials` (
+  `id` int NOT NULL,
   `user_id` int DEFAULT NULL,
   `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `credentials`
+--
+
+INSERT INTO `credentials` (`id`, `user_id`, `token`) VALUES
+(1, 1223224, '--type_your_api_token--');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `jira_credentials`
 --
 
 CREATE TABLE `jira_credentials` (
+  `id` int NOT NULL,
   `user_id` int NOT NULL,
   `api_token` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -39,13 +56,78 @@ CREATE TABLE `jira_credentials` (
   `basepath` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `jira_credentials`
+--
+
+INSERT INTO `jira_credentials` (`id`, `user_id`, `api_token`, `username`, `protocol`, `host`, `basepath`) VALUES
+(1, 3, '--type_your_api_token--', 'info@devsbeta.com', 'https', 'devsbeta.atlassian.net', '');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `trello_credentials`
 --
 
 CREATE TABLE `trello_credentials` (
+  `id` int NOT NULL,
   `user_id` int NOT NULL,
   `api_key` varchar(255) DEFAULT NULL,
   `api_token` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `trello_credentials`
+--
+
+INSERT INTO `trello_credentials` (`id`, `user_id`, `api_key`, `api_token`) VALUES
+(2, 2, '--type_your_api_key--', '--type_your_api_token--');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jira_credentials`
+--
+ALTER TABLE `jira_credentials`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trello_credentials`
+--
+ALTER TABLE `trello_credentials`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `credentials`
+--
+ALTER TABLE `credentials`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `jira_credentials`
+--
+ALTER TABLE `jira_credentials`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `trello_credentials`
+--
+ALTER TABLE `trello_credentials`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
